5381c076f3969fe6b22c1a28dceeb39ad8b6d469
Jon Phillips <jphillips@biaprotect.com>
2014-01-02 12:10:23 -0800

    re-fixed the nil problem

--- lib/resque/failure/multiple.rb
+++ lib/resque/failure/multiple.rb
@@ -26,33 +26,39 @@ module Resque
         @backends.each(&:save)
       end
 
+      def self.with_first_class(default)
+        return default unless classes.first
+
+        yield classes.first
+      end
+
       # The number of failures.
       def self.count(*args)
-        classes.first.count(*args)
+        with_first_class(0){|c| c.count(*args)}
       end
 
       # Returns a paginated array of failure objects.
       def self.all(*args)
-        classes.first.all(*args)
+        with_first_class([]){|c| c.all(*args)}
       end
 
       # Iterate across failed objects
       def self.each(*args, &block)
-        classes.first.each(*args, &block)
+        with_first_class(nil){|c| c.each(*args, &block)}
       end
 
       # A URL where someone can go to view failures.
       def self.url
-        classes.first.url
+        with_first_class(nil){|c| c.url}
       end
 
       # Clear all failure objects
       def self.clear(*args)
-        classes.first.clear(*args)
+        with_first_class(nil){|c| c.clear(*args)}
       end
 
       def self.requeue(*args)
-        classes.first.requeue(*args)
+        with_first_class(nil){|c| c.requeue(*args)}
       end
 
       def self.remove(index)
